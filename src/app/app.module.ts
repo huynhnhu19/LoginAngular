a83c7dca931ef7918af2a2c8f3a8459f12f59837
NhuDH <huynhnhudo10@gmail.com>
2019-10-22 10:19:37 +0700

    route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,30 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes =[
+  {path: 'home' , component: HomeComponent},
+  {path: 'about' , component: AboutComponent},
+  {path: 'contact' , component: ContactComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
