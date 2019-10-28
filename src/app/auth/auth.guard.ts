5061cf38d162a2bd77e8284522338cd196b6923d
NhuDH <huynhnhudo10@gmail.com>
2019-10-28 09:13:43 +0700

    edit login

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -8,25 +8,13 @@ import { AuthService } from './auth.service';
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) { }
-
-  // canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-  //   return this.authService.isLoggedIn().pipe(
-  //     map((isLoggedIn: boolean) => {
-  //       if (!isLoggedIn) {
-  //         this.router.navigate(['login']);
-  //         return false;
-  //       } else {
-  //         // this.router.navigate(['home']);
-  //         return true;
-  //       }
-  //     })
-  //   );
-  // }
+  
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    if (this.authService.isLoggedIn()) {
-      return true;
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate(['/login']);
+      return false;
     }
-    this.router.navigate(['/login']);
-    return false;
+    return true;
   }
+  
 }
