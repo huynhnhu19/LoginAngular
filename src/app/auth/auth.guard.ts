425c1c9128c75ed55468671d25af914f30024f00
NhuDH <huynhnhudo10@gmail.com>
2019-10-29 09:34:55 +0700

    edit name app

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -9,7 +9,7 @@ import { AuthService } from './auth.service';
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) { }
   
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     if (!this.authService.isLoggedIn()) {
       this.router.navigate(['/login']);
       return false;
